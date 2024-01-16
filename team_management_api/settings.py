4ff606c295a2fdec07b645bdce00495ca2d2c1ea
Veinmax <arturjust88@gmail.com>
2024-01-16 23:27:44 +0200

    added models for Team and Person

--- team_management_api/settings.py
+++ team_management_api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "rest_framework",
+    "team_management_service"
 ]
 
 MIDDLEWARE = [
@@ -106,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Kiev'
 
 USE_I18N = True
 
--- team_management_service/models.py
+++ team_management_service/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=50, unique=True)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
