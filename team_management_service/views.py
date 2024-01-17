f2d0d5af376f3b972092a6da06cf6ffec78aa716
Veinmax <arturjust88@gmail.com>
2024-01-17 10:59:25 +0200

    added serializers, views, urls

--- team_management_service/views.py
+++ team_management_service/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from team_management_service.models import Team, Person
+from team_management_service.serializers import TeamSerializer, PersonSerializer
 
-# Create your views here.
+
+class TeamViewSet(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+
+class PersonViewSet(viewsets.ModelViewSet):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
