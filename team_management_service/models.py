f30673fcfca8ebc89e90c25f7e2261023f94b745
Veinmax <arturjust88@gmail.com>
2024-01-17 11:05:05 +0200

    Fix NOT NULL constraint error on 'team_id' in person creation

--- team_management_service/models.py
+++ team_management_service/models.py
@@ -12,7 +12,7 @@ class Person(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(max_length=50, unique=True)
-    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
